1f9f2618c629559cd5324f04f52d3ad8cfa88132
talesagr <tales.aguiar@tech4log.com>
2024-11-18 11:18:23 -0300

    adicionando sync com o back de tickets do usuario

--- src/components/Tickets/index.jsx
+++ src/components/Tickets/index.jsx
@@ -1,11 +1,26 @@
 import './tickets.css';
 import { FaUser, FaLock } from "react-icons/fa";
-import { useState } from 'react';
+import {useEffect, useState} from 'react';
 import { Link } from 'react-router-dom';
 import { IoTicket } from "react-icons/io5";
 import { MdQrCode2 } from "react-icons/md";
+import axios from "axios";
 
 const Tickets = () => {
+    const [tickets, setTickets] = useState([]);
+    const userId = 1
+
+    useEffect(() => {
+        const fetchTickets = async () => {
+            try{
+                const response = await axios.get(`http://localhost:3001/ticket/user/${userId}`);
+                setTickets(response.data);
+            } catch (e) {
+                console.error("Erro ao buscar ingressos: ", e)
+            }
+        };
+        fetchTickets()
+    },[])
     return (
         <div className='container-tickets fade-in'>
             <div className='title-tickets'>
@@ -15,66 +30,27 @@ const Tickets = () => {
                 </div>
             </div>
             <div className='grid-tickets'>
-                <div className='box-tickets'>
-                    <div className='img-banner'>
-                        <img src="https://www.designi.com.br/images/preview/11807908.jpg" alt="" />
-                    </div>
-                    <div className='QRcode'>
-                        <MdQrCode2 />
-                    </div>
-                    <div className='tx-tickes'>
-                        <h5>LOTE 1</h5>
-                        <h5>PARTICIPANTE:</h5>
-                        <p>Caio Rolando</p>
-                        <h5>VALOR R$</h5>
-                        <p>R$35.00</p>
-                    </div>
-                </div>
-                <div className='box-tickets'>
-                    <div className='img-banner'>
-                        <img src="https://www.designi.com.br/images/preview/11807908.jpg" alt="" />
-                    </div>
-                    <div className='QRcode'>
-                        <MdQrCode2 />
-                    </div>
-                    <div className='tx-tickes'>
-                        <h5>LOTE 1</h5>
-                        <h5>PARTICIPANTE:</h5>
-                        <p>Caio Rolando</p>
-                        <h5>VALOR R$</h5>
-                        <p>R$35.00</p>
-                    </div>
-                </div>
-                <div className='box-tickets'>
-                    <div className='img-banner'>
-                        <img src="https://www.designi.com.br/images/preview/11807908.jpg" alt="" />
-                    </div>
-                    <div className='QRcode'>
-                        <MdQrCode2 />
-                    </div>
-                    <div className='tx-tickes'>
-                        <h5>LOTE 1</h5>
-                        <h5>PARTICIPANTE:</h5>
-                        <p>Caio Rolando</p>
-                        <h5>VALOR R$</h5>
-                        <p>R$35.00</p>
-                    </div>
-                </div>
-                <div className='box-tickets'>
-                    <div className='img-banner'>
-                        <img src="https://www.designi.com.br/images/preview/11807908.jpg" alt="" />
-                    </div>
-                    <div className='QRcode'>
-                        <MdQrCode2 />
-                    </div>
-                    <div className='tx-tickes'>
-                        <h5>LOTE 1</h5>
-                        <h5>PARTICIPANTE:</h5>
-                        <p>Caio Rolando</p>
-                        <h5>VALOR R$</h5>
-                        <p>R$35.00</p>
-                    </div>
-                </div>
+                {tickets.length > 0 ? (
+                    tickets.map(ticket => (
+                        <div key={ticket.id} className='box-tickets'>
+                            <div className='img-banner'>
+                                <img src="https://www.designi.com.br/images/preview/11807908.jpg" alt={ticket.eventName} />
+                            </div>
+                            <div className='QRcode'>
+                                <MdQrCode2 />
+                            </div>
+                            <div className='tx-tickes'>
+                                <h5>{ticket.eventName}</h5>
+                                <h5>PARTICIPANTE:</h5>
+                                <p>{ticket.participant || "Nome do Participante"}</p>
+                                <h5>VALOR R$</h5>
+                                <p>R${parseFloat(ticket.price).toFixed(2)}</p>
+                            </div>
+                        </div>
+                    ))
+                ) : (
+                    <p>Você ainda não possui ingressos.</p>
+                )}
             </div>
         </div>
     );
